702f8531085d7a75f60d8f61ed3b2cb641cccfdc
Jeff <jf.gasp@gmail.com>
2021-12-28 15:48:47 +0100

    create post

--- client/src/redux/actions/posts.actions.js
+++ client/src/redux/actions/posts.actions.js
@@ -5,8 +5,19 @@ export const getPosts = () => async (dispatch) => {
     try {
         // Récupération de tous les posts
         const { data } = await api.fetchPosts();
-        // Si type d'action = "FETCH_ALL", on envoie dans payload, nos datas
+        // Lorsque action.type = "FETCH_ALL", on envoie dans actions.payload, nos datas
         dispatch({ type: 'FETCH_ALL', payload: data });
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        // Création post
+        const { data } = await api.createPost(post);
+        // Lorsque action.type = "CREATE", on envoie dans actions.payload, notre post
+        dispatch({ type: 'CREATE', payload: data });
     } catch (error) {
         console.log(error.message);
     }
--- client/src/redux/reducers/posts.reducer.js
+++ client/src/redux/reducers/posts.reducer.js
@@ -1,11 +1,12 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default (posts = [], action) => {
+const initialPosts = [];
+
+export default function posts(posts = initialPosts, action) {
     switch (action.type) {
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
-            return posts;
+            return [...posts, action.payload];
         default:
             return posts;
     }
-};
+}

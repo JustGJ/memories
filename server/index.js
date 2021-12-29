0ee0454e7325efb5a2a5c5569645f51c77918c74
Jeff <jf.gasp@gmail.com>
2021-12-29 13:33:13 +0100

    commit heroku

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 // routes
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 // use est un moyen d'enregistrer un middleware ou une chaîne de middlewares (ou plusieurs middlewares) avant d'exécuter une logique de route finale
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
@@ -17,11 +19,13 @@ app.use(cors());
 // si on est sur /post, on exécute postRoutes
 app.use('/posts', postRoutes);
 
-const CONNEXION_URL =
-    'mongodb+srv://Jeff:admin@cluster0.lauwp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(CONNEXION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
     .catch((error) => console.log(error.message));
